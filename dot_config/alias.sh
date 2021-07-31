817139b1c9e8a2eaff3505cf28bd1c31fbf5740f
darius4691 <dariush4691@gmail.com>
2021-07-31 17:54:32 +0800

    Add dot_config/alias.sh

---
+++ dot_config/alias.sh
@@ -0,0 +1,11 @@
+alias top="htop"
+alias du="ncdu"
+alias vi="nvim"
+# alias tmux='tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf'
+alias fzf="fzf --preview 'bat --style=numbers --color=always {} | head -500'"
+alias box="boxes -d shell -a hcvcjc -s 80"
+alias la='exa -a'
+alias ll='exa -la'
+alias ls='exa'
+alias drawio='./opt/usr/drawio/drawio'
+alias nnn='nnn -a'
