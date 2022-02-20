b293153584770744f54e7fd3d68fdb25fd79cc1e
darius4691 <dariush4691@gmail.com>
2022-02-20 13:59:57 +0800

    Update dot_config/nvim/init.lua Update dot_config/nvim/lua/debugger.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,67 +1,180 @@
 -- vim:foldmethod=marker
+-- .nvimrc exmaple {{{
+--[[
+-- setting luasnip location; opt.paths gives a list of paths.
+-- The structure shold be like in https://github.com/rafamadriz/friendly-snippets
+-- containing a package.json
+require("luasnip.loaders.from_vscode").load(opts)
+
+-- setting debugger:
+local dap = require('dap')
+
+-- launch .vscode/launch.json file
+require('dap.ext.vscode').load_launchjs()
+
+-- python debugger {{{
+-- virtual environment is recommanded: pip install debugpy
+-- dap.adapters.python = {
+--   type = 'executable';
+--   command = "python";
+--   args = { '-m', 'debugpy.adapter' };
+-- }
+-- dap.configurations.python = {
+--   {
+--     -- The first three options are required by nvim-dap
+--     type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+--     request = 'launch';
+--     name = "Launch file";
+--
+--     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+--
+--     program = "${file}"; -- This configuration will launch the current file if used.
+--     pythonPath = function()
+--       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+--       -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+--       -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+--       local cwd = vim.fn.getcwd()
+--       if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+--         return cwd .. '/venv/bin/python'
+--       elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+--         return cwd .. '/.venv/bin/python'
+--       else
+--         return '/usr/bin/python'
+--       end
+--     end;
+--   },
+-- }
+-- }}}
+
+-- go debugger {{{
+-- install delve: go install github.com/go-delve/delve/cmd/dlv@latest
+-- install vscode-go: git clone https://github.com/golang/vscode-go && cd vscode-go && npm install && npm run compile
+-- dap.adapters.go = {
+--   type = 'executable';
+--   command = 'node';
+--   args = {os.getenv('HOME') .. '/dev/golang/vscode-go/dist/debugAdapter.js'};
+-- }
+-- dap.configurations.go = {
+--   {
+--     type = 'go';
+--     name = 'Debug';
+--     request = 'launch';
+--     showLog = false;
+--     program = "${file}";
+--     dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
+--   },
+-- }
+-- }}}
+
+-- c,cpp,rust debugger {{{
+-- download and unzip from https://github.com/microsoft/vscode-cpptools/releases
+-- dap.adapters.cppdbg = {
+--   id = 'cppdbg',
+--   type = 'executable',
+--   command = '/absolute/path/to/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+-- }
+-- dap.configurations.cpp = {
+--  {
+--    name = "Launch file",
+--    type = "cppdbg",
+--    request = "launch",
+--    program = function()
+--      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--    end,
+--    cwd = '${workspaceFolder}',
+--    stopOnEntry = true,
+--  },
+--  {
+--    name = 'Attach to gdbserver :1234',
+--    type = 'cppdbg',
+--    request = 'launch',
+--    MIMode = 'gdb',
+--    miDebuggerServerAddress = 'localhost:1234',
+--    miDebuggerPath = '/usr/bin/gdb',
+--    cwd = '${workspaceFolder}',
+--    program = function()
+--      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--    end,
+--  },
+-- }
+-- if GDB has pretty-printing support, include following setting in each configurations
+-- setupCommands = {
+--   {
+--      text = '-enable-pretty-printing',
+--      description =  'enable pretty printing',
+--      ignoreFailures = false
+--   },
+-- },
+-- re using configurations for c and rust
+-- dap.configurations.c = dap.configurations.cpp
+-- }}}
+
+--]]
+--}}}
 
 -- Defaults {{{
-vim.cmd("syntax enable") -- syntax highlight
-vim.opt.number = true -- show line numbers
+vim.opt.number = true                               -- show line numbers
 vim.opt.ruler = true
-vim.opt.ttyfast = true                          -- terminal acceleration
+vim.opt.ttyfast = true                              -- terminal acceleration
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.smarttab = true                               -- set tabs for a shifttabs logic
+vim.opt.smarttab = true                             -- set tabs for a shifttabs logic
 vim.opt.expandtab = true
-vim.opt.autoindent = true                             -- indent when moving to the next line while writing code
-vim.opt.cursorline = true                            -- shows line under the cursor's line
-vim.opt.showmatch = true                             -- shows matching part of bracket pairs (), [], {}
+vim.opt.autoindent = true                           -- indent when moving to the next line while writing code
+vim.opt.cursorline = true                           -- shows line under the cursor's line
+vim.opt.showmatch = true                            -- shows matching part of bracket pairs (), [], {}
 vim.opt.enc = "utf-8"                               -- utf-8 by default
 vim.opt.backspace = "indent,eol,start"              -- backspace removes all (indents, EOLs, start) What is start?
-vim.opt.scrolloff = 10                            -- let 10 lines before/after cursor during scroll
-vim.opt.clipboard = "unnamedplus"                       -- use system clipboard
-vim.opt.hidden = true                                  -- TextEdit might fail if hidden is not set.
-vim.opt.backup = false                                -- Some servers have issues with backup files, see #649.
+vim.opt.scrolloff = 10                              -- let 10 lines before/after cursor during scroll
+vim.opt.clipboard = "unnamedplus"                   -- use system clipboard
+vim.opt.hidden = true                               -- textEdit might fail if hidden is not set.
+vim.opt.backup = false                              -- some servers have issues with backup files, see #649.
 vim.opt.writebackup = false
-vim.opt.cmdheight = 2                             -- Give more space for displaying messages.
-vim.opt.updatetime = 300                          -- reduce updatetime (default is 4000 ms = 4 s) leads to noticeable
-vim.opt.iskeyword:append{"-"} -- treat dash separated words as a word text object"
-vim.opt.signcolumn = "yes"                        -- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved.
-vim.opt.showmode = false                              -- compatible with lightline
-vim.opt.showtabline = 2                           -- show tab line always
-vim.opt.list = true                                    -- show invisible characters
+vim.opt.cmdheight = 2                               -- give more space for displaying messages.
+vim.opt.updatetime = 300                            -- reduce updatetime (default is 4000 ms = 4 s) leads to noticeable
+vim.opt.iskeyword:append{"-"}                       -- treat dash separated words as a word text object"
+vim.opt.signcolumn = "yes"                          -- Always show the signcolumn, otherwise it would shift the text each time diagnostics appear/become resolved.
+vim.opt.showmode = false                            -- compatible with lightline
+vim.opt.showtabline = 2                             -- show tab line always
+vim.opt.list = true                                 -- show invisible characters
 vim.opt.listchars = "tab:>-,trail:~"                -- list symbols, extends,precedes are useless if warp is on
-vim.opt.foldnestmax = 1                           -- only fold top level
-vim.opt.foldmethod = "syntax"                       --fold by syntax
+vim.opt.foldnestmax = 1                             -- only fold top level
+vim.opt.foldmethod = "syntax"                       -- fold by syntax
 
-vim.opt.termguicolors = true
-vim.opt.completeopt = "menu,menuone,noselect"
-vim.opt.pumheight = 7
+vim.opt.termguicolors = true                        -- enable true color
+vim.opt.completeopt = "menu,menuone,noselect"       -- completion menu options
+vim.opt.pumheight = 7                               -- limit the completion menu height
 
-vim.api.nvim_set_var("loaded_python_provider", 0)  --- disable python2 support
-vim.cmd("autocmd FileType make set noexpandtab")        --change space back to tab
-vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber" )  -- disable line number in terminal mode
+vim.opt.exrc = true                                 -- enables loading .nvimrc or .exrc file in cwd
+vim.opt.secure = true                               -- do not exec write or command in .nvimrc or .exrc file
 
-vim.cmd( "highlight link CompeDocumentation NormalFloat" )
+vim.opt.cscopequickfix = "s-,c-,d-,i-,t-,e-,a-"
+vim.opt.cscopeverbose = false
+
+vim.api.nvim_set_var("loaded_python_provider", 0)   -- disable python2 support
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true})
-
-vim.go.cscopequickfix = "s-,c-,d-,i-,t-,e-,a-"
-vim.go.cscopeverbose = false
+vim.cmd("syntax enable")                            -- syntax highlight
+vim.cmd("autocmd FileType make set noexpandtab")    --change space back to tab
+vim.cmd("autocmd TermOpen * setlocal nonumber norelativenumber" )  -- disable line number in terminal mode
 
 --: }}}
 
--- Packer {{{
+-- Plugins and Packer {{{
 require('packer').startup(function(use)
     -- Packer itself
     use 'wbthomason/packer.nvim'
 
     -- LSP and debugger {{{
 
-    use {"L3MON4D3/LuaSnip"}
+    use {'L3MON4D3/LuaSnip'}
 
     use {
-        "hrsh7th/nvim-cmp",
+        'hrsh7th/nvim-cmp',
         requires = {
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lsp",
-            "quangnguyen30192/cmp-nvim-tags",
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-nvim-lsp',
+            'quangnguyen30192/cmp-nvim-tags',
             'saadparwaiz1/cmp_luasnip'
         },
         config = function()
@@ -75,7 +188,7 @@ require('packer').startup(function(use)
                 cmp.setup{
                     snippet = {
                         expand = function(args)
-                            require'luasnip'.lsp_expand(args.body)
+                            luasnip.lsp_expand(args.body)
                             vim.fn["vsnip#anonymous"](args.body)
                         end
                     },
@@ -113,15 +226,16 @@ require('packer').startup(function(use)
         end
     }
 
-    use {"neovim/nvim-lspconfig",
+    use {'neovim/nvim-lspconfig',
         config = function()
             local capabilities = vim.lsp.protocol.make_client_capabilities()
+            local lspconfig = require'lspconfig'
             capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-            require'lspconfig'.pyright.setup{capabilities=capabilities}
-            require'lspconfig'.gopls.setup{capabilities=capabilities}
-            require'lspconfig'.clangd.setup{capabilities=capabilities}
-            require'lspconfig'.jsonls.setup{capabilities=capabilities}
-            require'lspconfig'.sumneko_lua.setup{
+            lspconfig.pyright.setup{capabilities=capabilities}
+            lspconfig.gopls.setup{capabilities=capabilities}
+            lspconfig.clangd.setup{capabilities=capabilities}
+            lspconfig.jsonls.setup{capabilities=capabilities}
+            lspconfig.sumneko_lua.setup{
                 capabilities=capabilities,
                 settings = {
                     Lua = {
@@ -147,10 +261,11 @@ require('packer').startup(function(use)
         end
     }
 
+    use {'mfussenegger/nvim-dap'}
+
     -- LSP and debugger}}}
 
-
-    --- UI & theme {{{
+    -- UI and theme {{{
 
     use {'navarasu/onedark.nvim',
         config = function()
@@ -161,34 +276,36 @@ require('packer').startup(function(use)
     --- lualine
     use {'hoob3rt/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true},
-        config = function() require('lualine').setup{
-              options = {
-                theme = 'onedark',
-                section_separators = {'', ''},
-                component_separators = {'', ''},
-                icons_enabled = true,
-              },
-              sections = {
-                lualine_a = {{'mode',upper = true}},
-                lualine_b = {{'branch',icon = ''}},
-                lualine_c = {
-                    {'filename', file_status = true},
-                    {'diagnostics', sources = {"nvim_lsp"}},
+        config = function()
+            require('lualine').setup{
+                options = {
+                    theme = 'onedark',
+                    section_separators = {'', ''},
+                    component_separators = {'', ''},
+                    icons_enabled = true,
                 },
-                lualine_x = { 'encoding', 'fileformat', 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location'  },
-              },
-              inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename' },
-                lualine_x = { 'location' },
-                lualine_y = {  },
-                lualine_z = {   }
-              },
-              extensions = { 'fugitive' }
-        }end
+                sections = {
+                    lualine_a = {{'mode', upper = true}},
+                    lualine_b = {{'branch', icon = ''}},
+                    lualine_c = {
+                        {'filename', file_status = true},
+                        {'diagnostics', sources = {"nvim_lsp"}},
+                    },
+                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'},
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                extensions = {'fugitive'}
+        }
+    end
     }
     -- bufferline
     use {
@@ -200,14 +317,14 @@ require('packer').startup(function(use)
     }
     --- }}}
 
-    --- TOOLS
+    -- tools {{{
     -- git integration
     use 'tpope/vim-fugitive'
-    --- align
+    -- align
     use 'junegunn/vim-easy-align'
-    ---  <count>ai ii aI iI indent level
+    -- <count>ai ii aI iI indent level
     use 'michaeljsmith/vim-indent-object'
-    --- quoting/parenthesizing
+    -- quoting/parenthesizing
     use 'tpope/vim-surround'
     -- enable repeating supported plugin maps with .
     use 'tpope/vim-repeat'
@@ -223,10 +340,10 @@ require('packer').startup(function(use)
         end
         }
 
-    --- formatting
+    -- formatting
     use {'sbdchd/neoformat'}
 
-    --- Treesitter and highlight
+    -- Treesitter and highlight
     use {
         "nvim-treesitter/nvim-treesitter",
         run = ':TSUpdate',
@@ -259,18 +376,26 @@ require('packer').startup(function(use)
         end
     }
 
+    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+
     -- telescope
     use {"nvim-telescope/telescope.nvim",
         requires = {"nvim-lua/popup.nvim", "nvim-lua/plenary.nvim"},
     }
+    -- }}}
 
-    -- keymaps
+    -- Key Mappings {{{
     use {
         "folke/which-key.nvim",
         config = function()
             local wk = require("which-key")
             local ts = require("telescope.builtin")
             local bl = require("bufferline")
+            local dap = require("dap")
             wk.register({
                 --a = {require("dapui").toggle, "toggledapui"},
                 b = {ts.buffers, "Buffers"},
@@ -294,6 +419,9 @@ require('packer').startup(function(use)
                 r = {ts.lsp_references, "ListReferences"},
                 s = {vim.lsp.buf.rename, "RenameVariable"},
                 t = {ts.treesitter, "TreesitterObject"},
+                B = {dap.toggle_breakpoint, "DapBreak"},
+                C = {dap.continue, "DapContinue"},
+                O = {dap.repl.open, "DapREPL"},
                 M = {"<Cmd>TodoTelescope<Cr>", "TODOs"},
                 T = {vim.lsp.buf.formatting, "Formatting"},
                 ["."] = {ts.resume, "Resume"},
@@ -318,12 +446,7 @@ require('packer').startup(function(use)
             }})
       end
     }
-
-    use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},
-        config = function()
-        require('gitsigns').setup()
-    end
-    }
+    -- }}}
 
 end)
 --}}}
